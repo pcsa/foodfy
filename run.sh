912dcb0f06e88d05546fb84e7a1eb66e469e0364
Marlon Secundo <marlon_secundo@outlook.com>
2022-01-17 19:32:17 -0300

    feat: run and install scripts

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
+# /recipes
 cd recipes
 yarn dev > recipes.log 2>&1 &
-
 cd ..
 
-# cd auth
-# yarn dev > auth.log 2>&1 &
 
-# cd ..
+# /auth
+cd auth
+yarn dev > auth.log 2>&1 &
+cd ..
+
+# /
 yarn build:sass > sass.log 2>&1 &
 yarn nodemon
